ef5b7d7efaab31195c821ed4006161ef7b55d885
Varsha <varshajanaki15@gmail.com>
2025-12-23 20:22:31 +0530

    Fix socket.io backend URL typo

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -2,40 +2,44 @@ import { createContext, useContext, useEffect, useState } from "react";
 import { useAuthContext } from "./AuthContext";
 import io from "socket.io-client";
 
-const SocketContext =  createContext();
+const SocketContext = createContext();
 
 export const useSocketContext = () => {
-    return useContext(SocketContext);
+  return useContext(SocketContext);
 };
 
-export const SocketContextProvider = ({children}) => {
-    const [socket, setSocket] = useState(null);
-    const [onlineUsers, setOnlineUsers] = useState([]);
-    const {authUser} = useAuthContext();
+export const SocketContextProvider = ({ children }) => {
+  const [socket, setSocket] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const { authUser } = useAuthContext();
 
-    useEffect(() => {
-        if(authUser) {
-            const socket = io(' "https://chatbeta-backend.onrender.com', {
-                query: {
-                    userId: authUser._id,
-                },
-            });
+  useEffect(() => {
+    if (authUser) {
+      const socketInstance = io("https://chatbeta-backend.onrender.com", {
+        query: {
+          userId: authUser._id,
+        },
+        withCredentials: true,
+      });
 
-            setSocket(socket);
+      setSocket(socketInstance);
 
-            // socket.on() is used to listen to the events. can be used both on client side and on server side
-            socket.on("getOnlineUsers", (users) => {
-                setOnlineUsers(users);
-            });
+      socketInstance.on("getOnlineUsers", (users) => {
+        setOnlineUsers(users);
+      });
 
-            return () => socket.close();
-        } else {
-            if(socket) {
-                socket.close();
-                setSocket(null);
-            }
-        }
-    }, [authUser]);
+      return () => socketInstance.close();
+    } else {
+      if (socket) {
+        socket.close();
+        setSocket(null);
+      }
+    }
+  }, [authUser]);
 
-    return <SocketContext.Provider value={{socket, onlineUsers}}>{children}</SocketContext.Provider>
-}
+  return (
+    <SocketContext.Provider value={{ socket, onlineUsers }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
